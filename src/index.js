00538e8127adb5c9d84b815f96683a38e320d7a1
Sarhymar <sarhymar@gmail.com>
2022-07-04 18:50:20 -0400

    Añadiendo funcionalidad js, capturar valores y reversar la tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import validator from './validator.js';
+//Llamando al botón "Validar" y añadiendo evento
+const element= document.getElementById('buttonFunction');
 
-console.log(validator);
+element.addEventListener('click', buttonFunction);
+
+//LLamando a todos los valores del formulario
+function buttonFunction () {
+    let userName= document.getElementById('name').value
+    let userMail= document.getElementById('mail').value
+    let userPhone= document.getElementById('phone').value
+    let userAdress= document.getElementById('adress').value
+    
+    let cardNumber= document.getElementById('cardNumber').value
+    let cardCvv= document.getElementById('cardCvv').value
+    let cardDate= document.getElementById('date').value
+    let cardCountry= document.getElementById('country').value
+
+     
+    console.log(userName);
+    console.log(userMail);
+    console.log(cardDate);
+    console.log(cardNumber);
+
+    //Dando vuelta al número de tarjeta
+    const reverseCard = cardNumber.toString().split('').reverse();
+    console.log(reverseCard); 
+    }
+
+
+
+  /*  Probando la funcionalidad del botón
+    console.log(userName);
+    console.log(userMail);
+    console.log(cardDate);
+    console.log(cardNumber)*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,8 @@
 const validator = {
-  // ...
+  function (inputCard) {
+      const reverseCard = cardNumber.toString().split('').reverse().map(Number);
+      console.log(reverseInput); 
+  }
 };
 
 export default validator;
