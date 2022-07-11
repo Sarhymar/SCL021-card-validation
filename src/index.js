224adcd82ff3d80f568d4258494f55931e6df57d
Sarhymar <sarhymar@gmail.com>
2022-07-11 15:31:40 -0400

    Algoritmo terminado

--- src/index.js
+++ src/index.js
@@ -1,65 +1,35 @@
 import validator from './validator.js';
 //Llamando al botón "Validar" y añadiendo evento
-const element= document.getElementById('buttonFunction');
+const element = document.getElementById('buttonFunction');
 
 element.addEventListener('click', buttonFunction);
 
 
+
 //LLamando a todos los valores del formulario
-function buttonFunction () {
-    let userName= document.getElementById('name').value
-    let userMail= document.getElementById('mail').value
-    let userPhone= document.getElementById('phone').value
-    let userAdress= document.getElementById('adress').value
-    
+function buttonFunction() {
+  let userName = document.getElementById('name').value
+  let userMail = document.getElementById('mail').value
+  let userPhone = document.getElementById('phone').value
+  let userAdress = document.getElementById('adress').value
+
+  let cardNumber = parseInt(document.getElementById('cardNumber').value)
+  let cardCvv = document.getElementById('cardCvv').value
+  let cardDate = document.getElementById('date').value
+  let cardCountry = document.getElementById('country').value
+
+  /*function errorCardNumber () {   
     let cardNumber= parseInt (document.getElementById('cardNumber').value)
-    let cardCvv= document.getElementById('cardCvv').value
-    let cardDate= document.getElementById('date').value
-    let cardCountry= document.getElementById('country').value
-
-    /*function errorCardNumber () {   
-      let cardNumber= parseInt (document.getElementById('cardNumber').value)
-      if (cardNumber === NaN){
-        document.getElementById("demo").innerHTML+= 'Campo requerido, introduce solo números';
-  }
+    if (cardNumber === NaN){
+      document.getElementById("demo").innerHTML+= 'Campo requerido, introduce solo números';
 }
-*/
-    console.log(userName);
-    console.log(userMail);
-    console.log(cardDate);
-    console.log(cardNumber);
-
-       //Dando vuelta al número de tarjeta
-   let reversedCard = cardNumber.toString().split('').reverse().map(Number)
-   
-   //console.log(reversedCard);
-   
-   //
-   for(let i = 0; i < reversedCard.length; i++) {
-    if(i % 2 === 1) {
-      reversedCard[i] = reversedCard[i] * 2;
-      
-      let suma = reversedCard.length;
-      if (suma > 1) {
-        reversedCard[i] = reversedCard[i]-9; 
-        //reversedCard[i]+=reversedCard;
-    }
-     
-
-      }
-
-      
 }
 
-
-console.log(reversedCard);
-
-let arrayCard= reversedCard;
-let suma= 0;
-for(let i = 0;i<arrayCard.length; i++){
-  suma +=arrayCard[i];
+  console.log(userName);
+  console.log(userMail);
+  console.log(cardDate);
+  console.log(cardNumber);
+  */
+  validator.isValid(cardNumber); 
 }
 
-console.log(suma);
-
-}
--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,43 @@
 const validator = {
-  isValid(creditCardNumber) {
-  console.log(creditCardNumber)
-  }
+ isValid: function(cardNumber) {
+    //Dando vuelta al número de tarjeta
+    let reversedCard = cardNumber.toString().split('').reverse().map(Number)
   
+    console.log(reversedCard);
+  
+    //Encontrando posición impar y duplicándola
+    for (let i = 0; i < reversedCard.length; i++) {
+      if (i % 2 === 1) {
+        reversedCard[i] = reversedCard[i] * 2;
+        // Si el número duplicado tiene dos dígitos, resto nueve (mismo resultado de sumar ambos dígitos)
+        let digits = reversedCard[i];
+        if (digits >= 10) {
+          reversedCard[i] = reversedCard[i] - 9;
+        }
+      }
+    }
+  
+  
+    //Suma de todos los números del nuevo array de la tarjeta
+    let arrayCard = reversedCard;
+    let suma = 0;
+    for (let i = 0; i < arrayCard.length; i++) {
+      suma += arrayCard[i];
+    }
+  
+  
+    let lastDigit = suma.toString().slice(-1)
+    
+    if (lastDigit === '0' && suma > 0 && arrayCard.length > 15) {
+      console.log('Is valid');
+      alert('¡Sus datos han sido guardados con éxito!');
+    }
+    else {
+      console.log('Is not valid');
+      document.getElementById("demo").innerHTML = '';
+      document.getElementById("demo").innerHTML += 'Número de tarjeta inválido, revise el número ingresado';
+    }
+  }
 };
 
 export default validator;
